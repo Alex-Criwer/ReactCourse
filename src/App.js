9c7c4990d2b7c8f5494f1ec554fbde7e72c347c6
Golub Alexandra <golub.av@phystech.edu>
2022-09-29 17:43:04 +0300

    made made corrections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import mock_data from './assets/mock-data.json'
+import mockData from './assets/mock-data.json'
 import ProfileCardList from "./components/profile-card/profile-card-list";
 
 function App() {
   return (
     <div className="App">
-        <ProfileCardList cards={mock_data}/>
+        <ProfileCardList cards={mockData}/>
     </div>
   );
 }
--- src/components/profile-card/profile-card.jsx
+++ src/components/profile-card/profile-card.jsx
@@ -1,34 +1,30 @@
-import classes from './profile-card.module.css'
 import React, {useState} from "react";
+import classes from './profile-card.module.css'
 
 const ProfileCard = ({title, text, currentLikes}) => {
     const [counter, changeCounter] = useState(currentLikes)
     const [state, setState] = useState(false)
-    const [likeState, setLikeState] = useState("Like")
 
     const like = () => {
         if(state === true) {
             changeCounter(counter => counter - 1)
             setState(false)
-            setLikeState("Like")
             return
         }
 
         changeCounter(counter => counter + 1)
         setState(true)
-        setLikeState("Unlike")
     }
 
-    const changeColor = () => {
-        return state? "red" : "darkseagreen"
-    }
+    const changeColor = state ? "red" : "darkseagreen"
+
 
     return (
         <div className={classes.card}>
             <h1>{title}</h1>
             <div className={classes.textInfo}>{text}</div>
             <div className={classes.likes}>Likes: {counter}</div>
-            <div id='id' className={classes.button} onClick={like} style={{backgroundColor: changeColor() }}>{likeState}</div>
+            <div className={classes.button} onClick={like} style={{backgroundColor: changeColor }}>Like</div>
         </div>
     )
 }
